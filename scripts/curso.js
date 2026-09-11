0d9b62091b235fe1b6391326b0406d4a0a8a9925
gumam32-creator <gumam32@gmail.com>
2026-09-11 20:05:34 -0300

    Realizado a troca do arquivo js

--- scripts/curso.js
+++ scripts/curso.js
@@ -1,10 +1,10 @@
 const cursos = [
     { subject: 'CSE', number: 110, title: 'Introduction to Programming', credits: 1, completed: true },
     { subject: 'WDD', number: 130, title: 'Web Fundamentals', credits: 1, completed: true },
-    { subject: 'CSE', number: 111, title: 'Programming with Functions', credits: 1, completed: false },
-    { subject: 'CSE', number: 210, title: 'Programming with Classes', credits: 1, completed: false },
+    { subject: 'CSE', number: 111, title: 'Programming with Functions', credits: 1, completed: true },
+    { subject: 'CSE', number: 210, title: 'Programming with Classes', credits: 1, completed: true },
     { subject: 'WDD', number: 131, title: 'Dynamic Web Fundamentals', credits: 1, completed: true },
-    { subject: 'WDD', number: 231, title: 'Frontend Web Development I', credits: 1, completed: false }
+    { subject: 'WDD', number: 231, title: 'Frontend Web Development I', credits: 1, completed: true }
 ];
 
 const containerCursos = document.getElementById('container-cursos');
